a2080e6f44031b389d1b75bf196c9672cdc53349
m1-key <shubhamtiwari.tiwari84@gmail.com>
2021-10-10 01:06:46 +0530

    Website Update

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b000b727ac678274031c0b5ec3af0cce.js"
+  "/precache-manifest.7ec3a96f192cd100bbe80a97d92287eb.js"
 );
 
 workbox.clientsClaim();
